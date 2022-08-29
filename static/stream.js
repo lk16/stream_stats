938601675c310012ec111f00957723a213db90f8
Luuk Verweij <luuk_verweij@msn.com>
2022-08-29 18:14:08 +0200

    feat: support newlines properly

--- static/stream.js
+++ static/stream.js
@@ -1,11 +1,18 @@
-function test() {
+function load_stats() {
     $.ajax({
         url: "/stats",
         success: function(data) {
-            $("span").text(data.stats);
+            $("span").empty();
+            let split_data = data["stats"].split(/\r?\n/);
+            console.log(split_data);
+            for (var i = 0; i < split_data.length; i++) {
+                let line = split_data[i];
+                $("span").append(line);
+                $("span").append("<br/>");
+            }
         },
         dataType: "json",
     });
 }
 
-var interval = setInterval(test, 1000);
+var interval = setInterval(load_stats, 1000);
